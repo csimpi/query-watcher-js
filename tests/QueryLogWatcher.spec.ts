67ef57be26aa9c9095b5dee3dd9c9a24298ae283
pitt <pitt@jss.hu>
2022-09-09 00:31:03 -0700

    Add project description and repository, add pusher-js

--- tests/QueryLogWatcher.spec.ts
+++ tests/QueryLogWatcher.spec.ts
@@ -2,7 +2,6 @@ import {QueryLogWatcher} from "../src/QueryLogWatcher";
 
 describe('QueryLogWatcher', function() {
     it('initial_test', function() {
-        const queryLogWatcher = new QueryLogWatcher();
-        expect(queryLogWatcher.driver).toBe('redis');
+        //
     });
 });
